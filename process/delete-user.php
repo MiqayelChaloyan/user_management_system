04ba87160796b7b74e41f992ed701df6aeb18db7
MiqayelChaloyan <miqochaloyan@gmail.com>
2025-02-03 05:28:47 -0800

    -fix delete logic

--- process/delete-user.php
+++ process/delete-user.php
@@ -8,22 +8,33 @@ $response = [];
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
     $userId = (int) $_POST['id'];
 
-    // TODO: Delete local file
+    // Get the user file information
     $uploadDir = "../uploads/";
     $query = "SELECT file FROM users WHERE id = $userId";
     $result = mysqli_query($conn, $query);
     $row = mysqli_fetch_assoc($result);
-    $fileName = $row['file'];
-    $current_file_path = $uploadDir . $fileName;
+    
+    if ($row) {
+        $fileName = $row['file'];
 
-    if (file_exists($current_file_path)) {
-        unlink($current_file_path);
+        // Check if file exists and delete it
+        if ($fileName && file_exists($uploadDir . $fileName)) {
+            $current_file_path = $uploadDir . $fileName;
+            if (!unlink($current_file_path)) {
+                $response = [
+                    'status' => 500,
+                    'message' => 'Failed to delete the user image.'
+                ];
+                echo json_encode($response);
+                mysqli_close($conn);
+                exit;
+            }
+        }
     }
 
-    // TODO: Delete queries
+    // Delete user-related details and user
     $deleteDetailsQuery = "DELETE FROM user_details WHERE user_id = $userId";
     $deleteQuery = "DELETE FROM users WHERE id = $userId";
-
 
     if (mysqli_query($conn, $deleteDetailsQuery) && mysqli_query($conn, $deleteQuery)) {
         $response = [
